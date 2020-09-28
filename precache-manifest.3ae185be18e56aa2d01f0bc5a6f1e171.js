47663c6774245741cd5e43cac944f4007fdabe2a
J-Haze <59891689+J-Haze@users.noreply.github.com>
2020-09-27 20:06:35 -0700

    Updates

---
+++ precache-manifest.3ae185be18e56aa2d01f0bc5a6f1e171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fffb05a37ce3c995c8ccea6906c81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0683a5bac62bb48a5e",
+    "url": "/static/css/main.7d1c1d0b.chunk.css"
+  },
+  {
+    "revision": "75b17837ca28968bed61",
+    "url": "/static/js/2.ed460197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed460197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0683a5bac62bb48a5e",
+    "url": "/static/js/main.1d40550d.chunk.js"
+  },
+  {
+    "revision": "674ac8955cd16d5c8501",
+    "url": "/static/js/runtime-main.aad17668.js"
+  },
+  {
+    "revision": "3bcbc474a1776dea251b2e65f9247afa",
+    "url": "/static/media/battleship.3bcbc474.png"
+  },
+  {
+    "revision": "cb9575d06b3416c51162da082f24aae6",
+    "url": "/static/media/losing-image.cb9575d0.png"
+  },
+  {
+    "revision": "bbc758c88f41a082d070a2831863bb0a",
+    "url": "/static/media/winning-image.bbc758c8.png"
+  }
+]);
